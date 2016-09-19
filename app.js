2e815aae07f0c2d4c57256104fca7e29eed83d23
Matthias Schmidt <matthias.schmidt@dreebit.com>
2016-09-20 01:40:51 +0200

    template changes

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
+  app.use(express.favicon(__dirname + '/public/assets/favicon.ico'));
   app.use(app.router);
 });
 
